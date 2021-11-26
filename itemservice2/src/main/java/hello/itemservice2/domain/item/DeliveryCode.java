b6a2d3c0f74f907662f3e784a9906d6936fd0d71
youngwunjung <duddns6393@naver.com>
2021-11-26 15:13:44 +0900

    20211126 add deletion function

--- itemservice2/src/main/java/hello/itemservice2/domain/item/DeliveryCode.java
+++ itemservice2/src/main/java/hello/itemservice2/domain/item/DeliveryCode.java
@@ -3,10 +3,10 @@ package hello.itemservice2.domain.item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/**
- * FAST : 빠른 배송
- * NORMAL : 일반 배송
- * SLOW : 느린 배송
+/** 위치로 변경 : PositionCode(-)
+ * FAST : 빠른 배송   -> A : 생활용품
+ * NORMAL : 일반 배송 -> B : 식품
+ * SLOW : 느린 배송   -> C : 도서 및 잡화
  */
 @Data
 @AllArgsConstructor
